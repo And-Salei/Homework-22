68672e5f98665a77c612a596704cd90d2d39ee77
And-Salei <saleiandrew@gmail.com>
2022-01-25 18:24:26 +0200

    Ok!

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,13 +6,14 @@ else {index = m / ((h) ** 2);}
 index = Math.round (index * 10) / 10;
 
 
-if (index <= 16) {resultPlace = 'Выраженный дефицит массы тела';}
-if (index > 16 && index <=  18.5) {resultPlace = 'Недостаточная (дефицит) массы тела';}
-if (index > 18.5 && index <=  25) {resultPlace = 'Норма';}
-if (index > 25 && index <=  30) {resultPlace = 'Избыточная масса тела (предожирение)';}
-if (index > 30 && index <=  35) {resultPlace = 'Ожирение 1 степени';}
-if (index > 35 && index <=  40) {resultPlace = 'Ожирение 2 степени';}
-if (index > 40) {resultPlace = 'Ожирение 3 степени';}
+if (index <= 16) {result = 'Выраженный дефицит массы тела';}
+if (index > 16 && index <=  18.5) {result = 'Недостаточная (дефицит) массы тела';}
+if (index > 18.5 && index <=  25) {result = 'Норма';}
+if (index > 25 && index <=  30) {result = 'Избыточная масса тела (предожирение)';}
+if (index > 30 && index <=  35) {result = 'Ожирение 1 степени';}
+if (index > 35 && index <=  40) {result = 'Ожирение 2 степени';}
+if (index > 40) {result = 'Ожирение 3 степени';}
 resultPlaceIndex.innerHTML = index;
+resultPlace.innerHTML = result;
 
 }
